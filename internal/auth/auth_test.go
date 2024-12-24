ba981c997110ac18a263dd95b1b94579d9140b23
ansht2000 <ansht2000@gmail.com>
2024-12-23 21:26:56 -0800

    added tests for auth and slightly broke one of the tests to check if ci workflow catches it

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,44 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestAuth(t *testing.T) {
+	correctHeader := http.Header{}
+	incorrectHeaderMalformed := http.Header{}
+	incorrectHeaderNoAuth := http.Header{}
+	correctHeader.Add("Authorization", "ApiKey secretstuff")
+	incorrectHeaderMalformed.Add("Authorization", "secretstuff")
+
+	cases := []struct{
+		header http.Header
+		expectedAPIKey string
+		expectedError error
+	}{
+		{
+			header: correctHeader,
+			expectedAPIKey: "secretstuff",
+			expectedError: nil,
+		},
+		{
+			header: incorrectHeaderMalformed,
+			expectedAPIKey: "",
+			expectedError: ErrMalformedAuthHeader,
+		},
+		{
+			header: incorrectHeaderNoAuth,
+			expectedAPIKey: "",
+			expectedError: nil,
+		},
+	}
+
+	for i, c := range cases {
+		apiKey, err := GetAPIKey(c.header)
+		if apiKey != c.expectedAPIKey || err != c.expectedError {
+			t.Errorf("test %d: Failed to parse API key %s from header %v, instead got key %s", i + 1, c.expectedAPIKey, c.header, apiKey)
+			t.Fail()
+		}
+	}
+}
